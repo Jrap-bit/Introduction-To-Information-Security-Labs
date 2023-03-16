51a72b6b765c3457944347fb8ce27bc29703a205
jrap-bit <namanpandey10@gmail.com>
2023-03-16 10:16:29 +0530

    Implemented Feistel in DES - Temporary

--- 2-Mar-2023-DES.py
+++ 2-Mar-2023-DES.py
@@ -1,3 +1,36 @@
+def key_gen():
+    return '10110010'
+
+
+def feistel(block_64):
+    rounds = 1
+    key = key_gen()
+    left_block = block_64[:4]
+    right_block = block_64[4:]
+    for n in range(rounds):
+        xor_key_right = []
+        new_left = right_block
+        new_right = []
+        for x in range(len(left_block)):
+            xor_key_right.append(xor_8bit(right_block[x], key))
+        for i in range(len(left_block)):
+            new_right.append(xor_8bit(left_block[i], xor_key_right[i]))
+        left_block = new_left
+        right_block = new_right
+    feistel_ret = left_block + right_block
+    return feistel_ret
+
+
+def xor_8bit(func_a, func_b):
+    xor = ''
+    for i in range(8):
+        if func_a[i] == func_b[i]:
+            xor += '0'
+        else:
+            xor += '1'
+    return xor
+
+
 def divide_into_blocks(func_binary):
     block_mat = []
     for i in range(0, len(func_binary), 8):
@@ -16,10 +49,20 @@ def convert_to_binary(func_plaintext):
     return bin_mat
 
 
+def DES(DES_pt):
+    ciphertext = ''
+    binary = convert_to_binary(DES_pt)
+    # print(binary)
+    blocks = divide_into_blocks(binary)
+    # print(blocks)
+    feisteled = []
+    for i in blocks:
+        feistel_process = feistel(i)
+        feisteled.append(feistel_process)
+    print(feisteled)
+    return ciphertext
+
+
 if __name__ == "__main__":
     plaintext = input("Enter the plaintext: ")
-    binary = convert_to_binary(plaintext)
-    print(binary)
-    blocks = divide_into_blocks(binary)
-    print(blocks)
-    print(len(blocks))
+    ciphertext = DES(plaintext)
